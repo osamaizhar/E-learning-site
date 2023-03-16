f8feb24fabcc688554e84fdeb8c6930bacacabfb
osamaizhar <ahmedosamaizhar21@gmail.com>
2023-03-16 15:52:32 +0500

    added some more routing

--- e-learning/src/index.js
+++ e-learning/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter,Route,Routes } from "react-router-dom";
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+//import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
